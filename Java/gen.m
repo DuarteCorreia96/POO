f3f4a5082b113956d602f11b457084cb689816cb
Duarte Correia <duartecorreiascs@hotmail.com>
2019-05-09 17:15:15 +0100

    added UML

--- Java/gen.m
+++ Java/gen.m
@@ -1,14 +1,14 @@
 
 n = 30;
-a = randi([1 100], n, n);
+a = randi([1 30], n, n);
 
 %a(a > 29) = 0;
 
-fileID = fopen('test_3.xml','w');
+fileID = fopen('test_1.xml','w');
 
 fprintf(fileID, '<?xml version="1.0" encoding="UTF-8"?>\n');
 fprintf(fileID, '<!DOCTYPE simulation SYSTEM "simulation.dtd">\n');
-fprintf(fileID, '<simulation finalinst="300.0" antcolsize="200" plevel="0.5">\n');
+fprintf(fileID, '<simulation finalinst="500.0" antcolsize="10000" plevel="15">\n');
 fprintf(fileID, '	<graph nbnodes="%d" nestnode="1">\n',n);
 for i = 1:n
     fprintf(fileID, '		<node nodeidx="%d">\n', i);
@@ -22,8 +22,8 @@ end
 fprintf(fileID, '	</graph>\n');
 
 fprintf(fileID, '	<events>\n');
-fprintf(fileID, '		<move alpha="1.0" beta="1.0" delta="0.2"/>\n');
-fprintf(fileID, '		<evaporation eta="2.0" rho="10.0"/>\n');
+fprintf(fileID, '		<move alpha="1.0" beta="0.1" delta="0.1"/>\n');
+fprintf(fileID, '		<evaporation eta="15.0" rho="10.0"/>\n');
 fprintf(fileID, '	</events>\n');
 fprintf(fileID, '</simulation>\n');
 
